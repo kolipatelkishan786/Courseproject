9300fe42a58f82d23dcfe3ed52f4b38953f759c3
Kishan Patel <kolipatelkishan786@gmail.com>
2018-12-25 19:01:42 +0530

    addshopping component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,9 @@ import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +18,8 @@ import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component
     RecipesComponent,
     RecipeListComponent,
     RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
     RecipeItemComponent
   ],
   imports: [
