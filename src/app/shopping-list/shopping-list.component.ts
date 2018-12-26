22bb52da5692d20aba460fc1e9ba543ca177f5f1
Kishan Patel <kolipatelkishan786@gmail.com>
2018-12-26 17:26:40 +0530

    50. Working on the ShoppingListComponent

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients = [];
   constructor() { }
 
   ngOnInit() {
