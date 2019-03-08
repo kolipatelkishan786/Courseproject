2b112a7b9e381760d57ea454edc3778912934cf5
Kishan Patel <kolipatelkishan786@gmail.com>
2019-03-08 08:03:57 +0530

    207 Allowing the the User to Clear Cancel the Form.

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -46,8 +46,13 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.slService.addIngredient(newIngredient);
     }
-    this .editMode = false;
+    this.editMode = false;
     form.reset();
+  }
+
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
   }
 
   ngOnDestroy() {
