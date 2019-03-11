109c21bfd057ae70af07d4f2922fc220ff2e8cce
Kishan Patel <kolipatelkishan786@gmail.com>
2019-03-11 09:06:27 +0530

    219 Providing the Recipe Service Correctly.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
 import {RecipEditComponent} from './recipes/recip-edit/recip-edit.component';
+import {RecipeService} from "./recipes/recipe.service";
 
 @NgModule({
   declarations: [
@@ -36,7 +37,7 @@ import {RecipEditComponent} from './recipes/recip-edit/recip-edit.component';
     ReactiveFormsModule,
     FormsModule
   ],
-  providers: [ShoppingListService],
+  providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -5,8 +5,7 @@ import {RecipeService} from "./recipe.service";
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.scss'],
-  providers: [RecipeService]
+  styleUrls: ['./recipes.component.scss']
 })
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
