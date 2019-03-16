ed5bce83d67b6d535e526b9a49dc806dd2dc7d20
Kishan Patel <kolipatelkishan786@gmail.com>
2019-03-16 09:55:28 +0530

    271 Creating the Auth Feature Module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,21 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {ReactiveFormsModule} from "@angular/forms";
 import {RecipeService} from "./recipes/recipe.service";
 import {HttpClientModule} from "@angular/common/http";
 import {DataStorageService} from "./shered/data.storage.service";
-import {SignupComponent} from './auth/signup/signup.component';
-import {SigninComponent} from './auth/signin/signin.component';
 import {AuthService} from "./auth/auth.service";
 import {AuthGuardService} from "./auth/auth.guard.service";
 import {RecipesModule} from "./recipes/recipes.module";
 import {SharedModule} from "./shered/shared.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
+import {AuthModule} from "./auth/auth.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SignupComponent,
-    SigninComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +28,8 @@ import {ShoppingListModule} from "./shopping-list/shopping-list.module";
     ReactiveFormsModule,
     HttpClientModule,
     SharedModule,
-    ShoppingListModule
+    ShoppingListModule,
+    AuthModule
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuardService],
   bootstrap: [AppComponent]
